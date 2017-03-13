cc86bdbd3dc63dc5ef9fbcfc089f30483f49ed83
Pedro Lima <pedrolima.0000@gmail.com>
2017-03-13 01:43:03 +0000

    WUUT WUUT

--- robots/Robotrons/RobotAntiQuad.java
+++ robots/Robotrons/RobotAntiQuad.java
@@ -11,18 +11,19 @@ import java.util.HashMap;
 public class RobotAntiQuad extends  AdvancedRobot {
 
 	int obs;
-	int done = 0;
-	boolean cornerB = true, hitBot = false,Started = false;
-	static int corner = 270;
-	HashMap<String, ScannedRobotEvent> obstaculos;
+	
+	
+	private boolean  done = false,Started = true;
+	private HashMap<String, Event> obstaculos;
 	private Point2D old_position; // Robot's last position
 	private Point2D new_position; // New position
-	private Double distancia; // distancia do percurso
-
+	private double distanciaP = 0.0; // distancia do percurso
+	public double angulo;
 	public void run() {
 
 		
-	
+		old_position  = new Point((int) getX(),(int)getY());
+    	
 		// Set colors
 		obstaculos = new HashMap<>();
 		setBodyColor(new Color(221, 175, 19));
@@ -30,145 +31,167 @@ public class RobotAntiQuad extends  AdvancedRobot {
 		setRadarColor(new Color(99, 228, 199));
 		setBulletColor(new Color(255, 238, 0));
 		setScanColor(new Color(255, 241, 46));
-
+		
 		goCorner();
+		addCustomEvent(new MoveCompleteCondition(this));
+		Started = false;
+		distanciaP = 0.0;
 		FindObstacles();
-
+		
+		setTurnRight(50);
+		ahead(100);
+		execute();
+		goCorner();
+		done = true;
 		// iniciar scan /contagem
-
+		System.out.println("Percorrido: "+ distanciaP);
 	}
 //vai para o canto
 	public void goCorner() {
-		// We don't want to stop when we're just turning...
-		while (cornerB) {
-			// turn to face the wall to the "right" of our desired corner.
-			turnRight(normalRelativeAngleDegrees(corner - getHeading()));
-			// Move to that wall
-			ahead(5000);
-			// Turn to face the corner
-			turnLeft(90);
-			// Move to the corner
-			ahead(5000);
-			// Se bateu nao esta no canto verdadeiro e faz de novo
-			if (hitBot == false) {
-				cornerB = false;
+		// go to 0,0
+		while(getX() != 18 || getY() != 18){
+			turnLeft(getHeading());
+			double alpha = Math.toDegrees(Math.atan(getX()/getY()));
+			turnLeft(180 - alpha);
+			ahead(10000);
+			
+			if(alpha > 45) {
+				ahead(old_position.getX());
 			}
-			hitBot = false;
+			else {
+				ahead(old_position.getY());
+			}
 		}
 	}
-//procura inicial
+	
+	public void ProcuraBot(){
+		boolean doneS = false;
+		while(!doneS){
+			if(!Started){	
+				while(!Started){
+					turnRight(2);
+				}
+			} else {
+				Started = false;
+				doneS = true;
+			}
+		}
+	}
+//procura 
 	public void FindObstacles() {
 		turnRight(360 - getHeading());
 		obs = 0;
 		System.out.println("*******************ENCONTRAR ***************");
-		while (obs < 3) {
-			turnRadarRight(360);
+		while (obs < getOthers()) {
+			ProcuraBot();
+			obs++;
 		}
-		addCustomEvent(new MoveCompleteCondition(this));
-		done = obs;
-		Rotunda();
-
-	}
-	//procura os robots
-	public void FindNext() {
-	
-		if(done >0 ) {
-			Started = true;
-		obs = 0;
-		System.out.println("*******************Reprocurar ***************");
-		while (obs < 3) {
-			turnRadarRight(360);
-		}
-		Started = false;
-		Rotunda();
 		
-		}//else volta para o 0 0
+	
+		
+
 	}
-//ver se ateu enquanto ia para o canto
+	
+//bateu
 	public void onHitRobot(HitRobotEvent e) {
-		if (cornerB)
-			hitBot = true;
+		back(10);
+        turnLeft(90);
+		ahead(30);
+		turnRight(90);
+		ahead(30);
 
 	}
-
-	//scanned robot se ja estiver a percorrer comportasse diferente
+	
+	//paredes
+	public void onHitWall(HitWallEvent event) {
+	   	double ang = event.getBearing();
+		if(obstaculos.keySet().size() == 0){
+	   		if(ang > 0){
+				turnRight(ang);
+			   	turnLeft(90);
+	   		}
+		   	else {
+			   	turnLeft(-ang);
+				turnRight(90);
+		   	}
+		}
+		
+		if(obstaculos.keySet().size() == getOthers()){
+			if(ang > 0){
+				turnRight(ang);
+			   	turnLeft(90);
+	   		}
+		   	else {
+			   	turnLeft(-ang);
+				turnRight(90);
+		   	}
+			obstaculos.put("Last",event);
+			ahead(10000);
+		}
+   	}
+	
+	//scanned robot 
 	public void onScannedRobot(ScannedRobotEvent e) {
 	
-		if(Started == false && obs < 3){
 			
-		obstaculos.put(e.getName(), e);
-		}
-		else if( obs < 3){
-			if(obstaculos.containsKey(e.getName())){
-				obstaculos.put(e.getName(), e);
-				System.out.println("susbst");
+		
+		if(!Started && !(obstaculos.containsKey(e.getName()))){		
+			Started = true;	
+			
+			
+			if(obstaculos.size()>=1){
+				
+				//voltar a posicao
+				double h = getHeading() - angulo;
+				turnLeft(h);
+				//rodar 
+				double total = 300/(360/h);
+			
+				setTurnRight(h-7);
+				ahead(total);
+				execute();
+				
+				double distancia = Math.sqrt(Math.pow(60,2) + Math.pow(e.getDistance(),2));
+				ahead(distancia-total/4);
+			}
+			else {
+				
+				
+				double distancia = Math.sqrt(Math.pow(60,2) + Math.pow(e.getDistance(),2));
+				angulo = Math.toDegrees(Math.atan(60/e.getDistance()));
+				
+				turnLeft(angulo);
+				ahead(distancia + 30);
 			}
 			
+			obstaculos.put(e.getName(),e);
+			//Guardar o angulo para onde esta virado
+			angulo = getHeading();
 		}
-		obs++;
-	}
-
+		
+		if(done){
+			Started = true;
+			
+			/*******************************************************************/
 	
-	//vai ate um robot e contorna-o
-	public void vaiAte(ScannedRobotEvent ob) {
-		
-		//Vai ate ao robot 
-		setTurnRight(ob.getBearing());
-		setAhead(ob.getDistance()- 100);
-		
-		waitFor(new MoveCompleteCondition(this));
-		
-		//dalhe a vlta?
-		setTurnLeft(45);;
-		setAhead(80);
-		waitFor(new MoveCompleteCondition(this));
-		setTurnRight(45);
-		setAhead(50);
-		waitFor(new MoveCompleteCondition(this));
-		
-		//vai para o proximo
-		FindNext();
-
 	}
-
+	}
+	
+	
 	//Contar a distancia so quando vai andar a volta dos robots
 	public void onCustomEvent(CustomEvent e) {
 		
 		
-		if (cornerB == false) {
+		
 		new_position = new Point2D.Double(getX(), getY());
 		double euclidian = Math.sqrt(Math.pow((old_position.getX() - new_position.getX()), 2) + Math.pow((old_position.getY() - new_position.getY()), 2));
-        distancia += euclidian;
+        distanciaP += euclidian;
         old_position = new Point2D.Double(new_position.getX(), new_position.getY());
-       // System.out.println("Percorrido: "+ distancia);
-		}
+       
+		
 	}
 	
 	
-	//escolhe o prox robot para onde ir 
-	public void Rotunda() {
-		
-		old_position = new Point2D.Double(getX(), getY());
-	    distancia = 0.0;
-		String nome = new String();
-		double aux = getBattleFieldWidth();
-		
-		for (String a : obstaculos.keySet()) {
-			ScannedRobotEvent px = obstaculos.get(a);
-			if(px.getDistance() < aux ){
-				nome = a;
-				aux = px.getDistance();
-			}
-				System.out.println("E o : " + a);
-		}
-		ScannedRobotEvent ob1 = obstaculos.get(nome);
-		obstaculos.remove(nome);
-		done--;
-		vaiAte(ob1);
-		
-
-
-	}
 	
 	
 
