d0c2ba18f78eee9c4d39b670d334d93752f97980
Pedro Lima <pedrolima.0000@gmail.com>
2017-03-19 22:19:37 +0000

    palyer vazio

---
+++ Poker/src/Player.java
@@ -0,0 +1,19 @@
+
+import jade.core.*;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+
+public class Player  extends Agent{
+
+	protected void setup(){
+		super.setup();
+		
+	}
+	
+
+}
