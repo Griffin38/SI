2cf6fbcd779a22929683af9019883adf4658aa79
Pedro Lima <pedrolima.0000@gmail.com>
2017-03-19 22:18:46 +0000

    Main add agentes dinamicamente

---
+++ Poker/src/Main.java
@@ -0,0 +1,40 @@
+import jade.core.*;
+import jade.core.Runtime;
+import jade.wrapper.*;
+public class Main {
+	Runtime rt;
+	ContainerController container;
+	public void initMainContainer(String host, String port) {
+	this.rt = Runtime.instance();
+	Profile prof = new ProfileImpl();
+	prof.setParameter(Profile.MAIN_HOST, host);
+	prof.setParameter(Profile.MAIN_PORT, port);
+	prof.setParameter(Profile.MAIN, "true");
+	prof.setParameter(Profile.GUI, "true");
+	this.container = rt.createMainContainer(prof);
+	rt.setCloseVM(true);
+	}
+	public static void main(String[] args) {
+		
+		Main mc = new Main();
+		mc.initMainContainer("127.0.0.1", "1099");
+		mc.startAgentInPlatform("Dealer", "Dealer");
+		//input quantos jogadores
+		int x = 3;
+		for(int i= 0;i<x; i++){
+			mc.startAgentInPlatform("Jogador "+ i, "Player");
+		}
+	}
+	
+	public void startAgentInPlatform(String name, String classpath){
+		try {
+		AgentController ac = container.createNewAgent(
+		name,
+		classpath,
+		new Object[0]);
+		ac.start();
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+		}
+}
