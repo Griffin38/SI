f9acc3dc5a2c3143ba740b61d16f2114945aef65
Pedro Lima <pedrolima.0000@gmail.com>
2017-03-28 15:48:20 +0100

    esqueleto decisao

--- Poker/main/AgentPlayer.java
+++ Poker/main/AgentPlayer.java
@@ -1,210 +1,231 @@
-package main;
+	package main;
 
 
-import cartas.Card;
-import cartas.IPlayer;
-import jade.core.*;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.OneShotBehaviour;
-import jade.core.behaviours.SimpleBehaviour;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.MessageTemplate;
-import java.util.ArrayList;
-import java.util.List;
+	import cartas.Card;
+	import cartas.IPlayer;
+	import jade.core.*;
+	import jade.core.behaviours.CyclicBehaviour;
+	import jade.core.behaviours.OneShotBehaviour;
+	import jade.core.behaviours.SimpleBehaviour;
+	import jade.lang.acl.ACLMessage;
+	import jade.lang.acl.MessageTemplate;
+	import java.util.ArrayList;
+	import java.util.List;
 
 
-public class AgentPlayer  extends Agent{
-        private double dinheiro;
-        private int nrAgentesFold;
-        private IPlayer jogador;
-       private List<Card> table;
-    
-        
-        @Override
-    protected void setup() {
-        super.setup();
-        this.nrAgentesFold=0;
-        this.dinheiro=0;
-        this.addBehaviour(new SendMessageEntrance() );
-       
-        
-        }
-    
-
-        
-   
-   
-    
-private class SendMessageEntrance extends OneShotBehaviour{
-		
-		public sendMessageEntrance(){
-		jogador = new Player(this.getName());
-		}
-		@Override 
-		public void action(){
-			AID receiver = new AID();
-			receiver.setLocalName("Dealer");
-			ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-			msg.setOntology(Ontologias.ENTRAR);
-			try {
-				msg.setContentObject(this.jogador);
-				msg.addReceiver(receiver);
-				myAgent.send(msg);
-				this.addBehaviour(new ReceiveBehaviourJogador());
-			} catch (Exception e) {
-				
-			System.out.println(e.getMessage());
-			}
-			
-		}
-		
-		
-	}
-    
- 	/************************************************* INICIO DE UMA MAO *************************************************/ 
-    private class ReceiveBehaviourJogador extends  CyclicBehaviour {
-                
-		
-		@Override
-		public void action(){
-	
-			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-			MessageTemplate mtJ = MessageTemplate.MatchOntology(Ontologias.NOVAMAO);
-			MessageTemplate mtResp = MessageTemplate.and(mt, mtJ);
-			ACLMessage msg = receive(mtResp);
-			
-			if(msg != null){
-				
-				try {
-					table = new List<Card>();
-					SequentialBehaviour seq = new SequentialBehaviour();
-					 seq.addSubBehaviour(new NewCards());
-					 seq.addSubBehaviour(new GetFlop());
+	public class AgentPlayer  extends Agent{
+					private double dinheiro;
+					private int nrAgentesFold;
+					private IPlayer jogador;
+				private List<Card> table;
+			private int myRank;
 					
-                                       
-                                        
-                                      
-
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
+					@Override
+			protected void setup() {
+					super.setup();
+					this.nrAgentesFold=0;
+					this.dinheiro=0;
+					this.addBehaviour(new SendMessageEntrance() );
 				
-			}else
-			block();
-				
-			}
-
-        
-		}
-    
-    
-private class NewCards extends OneShotBehaviour{
-	@Override
-		public void action(){
-				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-			MessageTemplate mtJ = MessageTemplate.MatchOntology(Ontologias.CARTAS);
-			MessageTemplate mtResp = MessageTemplate.and(mt, mtJ);
-			ACLMessage msg = receive(mtResp);
-			
-			if(msg != null){
-				
-				try {
-					Card[] cards = new Card[2];
-					//tira as cards do content
-					jogador.setCards(cards);
-                                      
-                                        
-                                      
-
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				
-			}else
-			block();
-	}
-
-}
-    
- private class GetFlop extends OneShotBehaviour{
-	@Override
-		public void action(){
-				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
-			MessageTemplate mtJ = MessageTemplate.MatchOntology(Ontologias.FLOP);
-			MessageTemplate mtResp = MessageTemplate.and(mt, mtJ);
-			ACLMessage msg = receive(mtResp);
-			
-			if(msg != null){
-				
-				try {
 					
-                          table =msg.getContentObject(); 
-						           
-                           this.addBehaviour(new PlayGame());             
-                                      
+					}
+			
 
+					
+		
+		
+			
+	private class SendMessageEntrance extends OneShotBehaviour{
+			
+			public sendMessageEntrance(){
+			jogador = new Player(this.getName());
+			}
+			@Override 
+			public void action(){
+				AID receiver = new AID();
+				receiver.setLocalName("Dealer");
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.setOntology(Ontologias.ENTRAR);
+				try {
+					msg.setContentObject(this.jogador);
+					msg.addReceiver(receiver);
+					myAgent.send(msg);
+					this.addBehaviour(new ReceiveBehaviourJogador());
 				} catch (Exception e) {
-					System.out.println(e.getMessage());
+					
+				System.out.println(e.getMessage());
 				}
 				
-			}else
-			block();
-	}
-
-}   
-/************************************************* DECISAO *************************************************/
-	private class PlayGame extends SimpleBehaviour{
-		private boolean finished = false;
-		int rank ,round,bet,pot;
-		public PlayGame(){
-			rank=0;
-			round = 0;
-			bet = 0;
-			pot= 0;
-
-		}
-			public voi action(){
-				//se o dealer falar decidir
 			}
-
+			
+			
+		}
+			
+		/************************************************* INICIO DE UMA MAO *************************************************/ 
+			private class ReceiveBehaviourJogador extends  CyclicBehaviour {
+									
+			
 			@Override
-			public boolean done() {
+			public void action(){
+		
+				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+				MessageTemplate mtJ = MessageTemplate.MatchOntology(Ontologias.NOVAMAO);
+				MessageTemplate mtResp = MessageTemplate.and(mt, mtJ);
+				ACLMessage msg = receive(mtResp);
 				
-				return finished;
+				if(msg != null){
+					
+					try {
+						table = new List<Card>();
+						SequentialBehaviour seq = new SequentialBehaviour();
+						seq.addSubBehaviour(new NewCards());
+						seq.addSubBehaviour(new GetFlop());
+						
+																				
+																					
+																				
+
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					
+				}else
+				block();
+					
+				}
+
+					
 			}
-	}    
-    
-    
-    
-	/************************************************* Metodos *************************************************/    
-    public double getDinheiro() {
-        return dinheiro;
-    }
+			
+			
+	private class NewCards extends OneShotBehaviour{
+		@Override
+			public void action(){
+				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+				MessageTemplate mtJ = MessageTemplate.MatchOntology(Ontologias.CARTAS);
+				MessageTemplate mtResp = MessageTemplate.and(mt, mtJ);
+				ACLMessage msg = receive(mtResp);
+				
+				if(msg != null){
+					
+					try {
+						Card[] cards = new Card[2];
+						//tira as cards do content
+						jogador.setCards(cards);
+																				
+																					
+																				
 
-    public void setDinheiro(double dinheiro) {
-        this.dinheiro = dinheiro;
-    }
-	
-    
-    public void retirarDinheiro(double quantidade) {
-    
-        this.dinheiro=this.dinheiro-quantidade;
-    }
-    
-    
-    public void aumentaDinheiro(double quantidade) {
-    
-        this.dinheiro=this.dinheiro+quantidade;
-        
-    }
-    
-    
-    
-    
-   
-    
-    
-    
-    
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					
+				}else
+				block();
+		}
 
-}
+	}
+			
+	private class GetFlop extends OneShotBehaviour{
+		@Override
+			public void action(){
+				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+				MessageTemplate mtJ = MessageTemplate.MatchOntology(Ontologias.FLOP);
+				MessageTemplate mtResp = MessageTemplate.and(mt, mtJ);
+				ACLMessage msg = receive(mtResp);
+				
+				if(msg != null){
+					
+					try {
+						
+								table =msg.getContentObject(); 
+								checkRanking(jogador,table);
+								       
+								this.addBehaviour(new PlayGame(RankingUtil.getRankingToInt(jogador)));             
+																				
+
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+					
+				}else
+				block();
+		}
+
+	}   
+	/************************************************* DECISAO *************************************************/
+		private class PlayGame extends SimpleBehaviour{
+			private boolean finished = false;
+			int rank ,round,bet,pot;
+			public PlayGame(int rankflop){
+				rank=rankflop;
+				round = 0;
+				bet = 0;
+				pot= 0;
+
+			}	
+				public voi action(){
+					ACLMessage msg = receive();
+					if(msg != null){
+						if(msg.getOntology().equals(Ontologias.PERGUNTAR)){
+							//check rank && round preço para entrar 
+							round++;
+						}else if(msg.getOntology().equals(Ontologias.TURN)){
+
+						}else if (msg.getOntology().equals(Ontologias.RIVER)){
+
+						}else if (msg.getOntology().equals(Ontologias.RAISE)){
+
+						}else if(msg.getOntology().equals(Ontologias.PERDEU)){
+							if(dinheiro == 0){ 
+								//add pedir para sair 
+							}
+							finished = true;
+						}else if(msg.getOntology().equals(Ontologias.DINHEIRO)){
+							//add dinheiro ao total 
+							finished = true;
+						}
+					}else block();
+				}
+
+				@Override
+				public boolean done() {
+					
+					return finished;
+				}
+		}    
+			
+			
+			
+		/************************************************* Metodos *************************************************/    
+			public double getDinheiro() {
+					return dinheiro;
+			}
+
+			public void setDinheiro(double dinheiro) {
+					this.dinheiro = dinheiro;
+			}
+		
+			
+			public void retirarDinheiro(double quantidade) {
+			
+					this.dinheiro=this.dinheiro-quantidade;
+			}
+			
+			
+			public void aumentaDinheiro(double quantidade) {
+			
+					this.dinheiro=this.dinheiro+quantidade;
+					
+			}
+			
+			
+			
+			
+		
+			
+			
+			
+			
+
+	}
