60f63127185d872cca880da1e2fd5c49c1bfad1f
Ultimate <a67738@alunos.uminho.pt>
2017-06-04 21:36:08 +0100

    Completo

---
+++ Robocode/robots/sampleteam/General.java
@@ -0,0 +1,246 @@
+package sampleteam;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.shape.Circle;
+import robocode.BulletHitEvent;
+import robocode.BulletMissedEvent;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.RobotDeathEvent;
+import robocode.ScannedRobotEvent;
+import robocode.TeamRobot;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+/**
+ *
+ * @author gil
+ */
+public class General extends TeamRobot {
+    
+    private Map<String,String> posicao;
+    private final String FILHO ="Filho";
+    public void run() {
+    	
+		setColors(Color.BLACK,Color.BLACK,Color.BLACK); // body,gun,radar
+		setBulletColor(Color.WHITE);
+                this.posicao= new HashMap<>();
+                
+                while(true) {
+		
+                    
+			turnGunLeft(360);
+			
+			
+			System.out.println("-------- BattleField Info ---------");
+			
+			
+                        System.out.println( " número de aliados "+this.posicao.size());
+                        
+		
+			System.out.println("> My position: " + (int)getX() + "," + (int)getY());
+			
+			
+			System.out.println("------------------------------------");
+			
+			
+		}
+           
+                
+    }
+    
+    
+    public void smartFire(double robotDistance) {
+		if (robotDistance > 200 || getEnergy() < 15) {
+			fire(2);
+		} else if (robotDistance > 50) {
+			fire(2);
+		} else {
+			fire(3);
+		}
+	}
+       
+    @Override
+        public void onHitByBullet(HitByBulletEvent e) {
+    	//ver vida do robot 
+    	//se tiver pouca pedir ajuda
+    	if(this.getEnergy() < 100){
+  	  try {
+			broadcastMessage(new String("Ajuda"+":"+getX()+","+getY()));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}}
+		turnRight(30);
+		ahead(90);
+	}
+        
+        
+   @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+	 
+        double aliadoBearing = this.getHeading() + e.getBearing();
+	int  aliadoX = (int)( getX() + e.getDistance() * Math.sin(Math.toRadians(aliadoBearing)));
+        int aliadoY = (int) (getY() + e.getDistance() * Math.cos(Math.toRadians(aliadoBearing)));
+
+       
+        
+        if(isTeammate(e.getName()) ==false){
+            
+                    try {
+                               
+                        broadcastMessage(new String("NE"+":"+e.getName()));
+                        } catch (IOException ex) {
+				System.out.println(ex.getMessage());
+			}
+                        
+
+                double absoluteBearing = getHeading() + e.getBearing();
+		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+                
+		if (Math.abs(bearingFromGun) <= 3) {
+			turnGunRight(bearingFromGun);
+
+			if (getGunHeat() == 0 && safeToShoot(aliadoX,aliadoY)) {
+				try {
+					broadcastMessage(new String("Atacar"+":"+aliadoX+","+aliadoY));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				smartFire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+				ahead(40);
+			}
+                        
+                        else {
+                                 turnRight(120);
+                                ahead(200);
+                        
+                        }
+		}
+		else {
+			
+                    turnGunRight(bearingFromGun);
+		}
+		if (bearingFromGun == 0) {
+			scan();
+		
+                    }
+                
+                
+			
+                 }
+        
+        else {
+        
+               this.posicao.put(FILHO,new String((int)aliadoX+","+(int)aliadoY));
+               turnRight(90);
+               ahead(200);
+            
+        }
+        
+       
+        
+}              
+       
+     @Override
+	public void onHitWall(HitWallEvent e) {
+		turnRight(180);
+		ahead(70);
+	}
+
+	public void OnBulletMissed(BulletMissedEvent e) {
+		turnLeft(30);
+		ahead(90);
+	}
+                              
+   @Override
+          public void onBulletHit(BulletHitEvent event) {
+       
+              if(isTeammate(event.getName())){
+		   turnRight(120);
+		   ahead(200);
+	   }
+          }
+          
+    
+@Override
+public void onHitRobot(HitRobotEvent event) {
+           
+           if(isTeammate(event.getName())){
+		   turnRight(90);
+		   ahead(20);
+	   }
+           
+}  
+      
+    
+    
+    public boolean safeToShoot(int enemyX,int enemyY){
+    
+        String[] c;
+	Circle circulo = new Circle(enemyX, enemyY, 10);
+        
+        if(this.posicao.isEmpty()==false) {
+        
+            c = posicao.get(FILHO).split(",");
+            int x = Integer.parseInt(c[0]);
+            int y = Integer.parseInt(c[1]);
+
+
+            if(Math.sqrt(Math.pow(x-circulo.getCenterX(),2)+Math.pow(y-circulo.getCenterY(), 2)) <Math.pow(circulo.getRadius(),2)) {
+             System.out.println("Não é seguro disparar ;)");
+                return false;  
+            }
+            
+             circulo = new Circle((int)getX(), (int) getY(), 10);
+             
+             c = posicao.get(FILHO).split(",");
+                x = Integer.parseInt(c[0]);
+                y = Integer.parseInt(c[1]);
+               if(Math.sqrt(Math.pow(x-circulo.getCenterX(),2)+Math.pow(y-circulo.getCenterY(), 2)) < Math.pow(circulo.getRadius(),2)){
+                System.out.println("Não é seguro disparar");
+                   return false;
+               }
+            
+        }
+       return true;
+    
+    }
+    
+    
+    @Override
+    public void onRobotDeath(RobotDeathEvent e) {
+    
+        if(isTeammate(e.getName())){
+            this.posicao.clear();
+      	  try {
+				broadcastMessage(new String("Aliado Morto" +":"));
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+        }else {
+        	 try {
+ 				broadcastMessage(new String("Enemigo Morto"+":"));
+ 			} catch (IOException ex) {
+ 				ex.printStackTrace();
+ 			}
+        }
+        
+    
+    }
+        
+        
+        
+    
+    
+    
+}
---
+++ Robocode/robots/sampleteam/Soldado.java
@@ -0,0 +1,333 @@
+package sampleteam;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.awt.Color;
+import java.util.*;
+import robocode.BulletHitEvent;
+import robocode.BulletMissedEvent;
+import robocode.Droid;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.MessageEvent;
+import robocode.RobotDeathEvent;
+import robocode.TeamRobot;
+import robocode.TurnCompleteCondition;
+
+/**
+ *
+ * @author gil
+ */
+public class Soldado extends TeamRobot {
+    
+    public double lastX = 0.0;
+    public double lastY = 0.0;
+    private List<String> nomeEnimigos;
+    boolean movingForward;
+    private double energia;
+    boolean go;
+    int courage;
+    int personality;
+    private int minperso;
+    public double distanceRobot = 0.0;
+	public double angulo;
+    @Override
+    public void run() {
+    	 this.energia=getEnergy();
+        this.nomeEnimigos=new ArrayList<>();
+        /****************************************************************************************/
+        Random r = new Random();
+        //couragem base 
+        this.courage =  r.nextInt(100) + 40;
+        
+        //personalidades
+        this.personality = r.nextInt(3) + 1;
+        switch(this.personality){
+        //Medricas
+        case 1: setGunColor(Color.green);
+        	this.personality = 15;
+        		this.minperso = 5;
+        		break;
+        //Normal
+        case 2:setGunColor(Color.white);
+        	this.personality = 10;
+        		this.minperso = 3;
+        		break;
+        //Heroi
+        case 3 :setGunColor(Color.orange);
+        	this.minperso = 1;
+        		break;
+        }
+        /****************************************************************************************/
+        
+    	lastX = getX();
+    	lastY = getY();
+	
+    	setBodyColor(Color.green);
+	
+        while(true) {
+          go =false;
+       /*
+            while(this.nomeEnimigos.size()>1) 
+                goTo();
+       */
+            //System.out.println(this.nomeEnimigos.size());
+            //go=false;
+           
+            
+            setAhead(40000);
+            movingForward = true;
+            setTurnRight(90);
+            waitFor(new TurnCompleteCondition(this));
+            // Note:  We are still moving ahead now, but the turn is complete.
+            // Now we'll turn the other way...
+            setTurnLeft(180);
+            // ... and wait for the turn to finish ...
+            waitFor(new TurnCompleteCondition(this));
+            // ... then the other way ...
+            setTurnRight(180);
+            // .. and wait for that turn to finish.
+            waitFor(new TurnCompleteCondition(this));
+           
+        }
+       
+       
+       
+       
+    }
+    
+    public void smartFire(double robotDistance) {
+		if (robotDistance > 200 || getEnergy() < 15) {
+			fire(2);
+		} else if (robotDistance > 50) {
+			fire(2);
+		} else {
+			fire(3);
+		}
+	}
+    
+    
+    @Override
+        public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(30);
+		ahead(90);
+		 this.courage = this.courage - this.personality;
+	}
+    
+     @Override
+   public void onHitWall(HitWallEvent e) {
+		// Bounce off!
+    	 this.courage = this.courage - this.minperso;
+                if(go==false) { 
+		turnRight(90);
+		   ahead(200);
+                }
+	}
+
+	/**
+	 * reverseDirection:  Switch from ahead to back & vice versa
+	 */
+	public void reverseDirection() {
+		if (movingForward) {
+			setBack(40000);
+			movingForward = false;
+		} else {
+			setAhead(40000);
+			movingForward = true;
+		}
+	}
+
+	public void OnBulletMissed(BulletMissedEvent e) {
+		turnLeft(30);
+		ahead(90);
+		 this.courage = this.courage - this.minperso;
+	}
+                              
+   @Override
+          public void onBulletHit(BulletHitEvent event) {
+       
+              if(isTeammate(event.getName())){
+		   turnRight(120);
+		   ahead(200);
+		   this.courage = this.courage - this.personality;
+	   }else {
+    	   this.courage = this.courage + this.personality;
+        turnRight(90);
+	   ahead(200);
+           
+       }
+          }
+    
+    
+    
+    
+     @Override
+    public void onHitRobot(HitRobotEvent event) {
+           
+           if(isTeammate(event.getName())){
+		   turnRight(100);
+		   ahead(20);
+		   this.courage = this.courage - this.minperso;
+	   }
+           
+           
+           else {
+        	   this.courage = this.courage + this.minperso;
+            turnRight(90);
+		   ahead(200);
+               
+           }
+           
+           }
+    
+    
+        
+    
+    private void colorCheck(){
+    	
+    	if(this.courage < 60 && this.courage > 35){
+    		setBodyColor(Color.yellow) ;
+
+    	}else if(this.courage <=35){
+    		setBodyColor(Color.red) ;
+    	}else {setBodyColor(Color.green) ;}
+    }
+    
+    private void goTo(){
+		// go to 0,0
+    	this.colorCheck();
+                
+		while(getX() != 18 || getY() != 18 ){
+                    if(this.nomeEnimigos.size()<=1)
+                        break;
+                    else {  
+                    go=true;
+			turnLeft(getHeading());
+			double alpha = Math.toDegrees(Math.atan(getX()/getY()));
+			turnLeft(180 - alpha);
+			ahead(10000);
+			
+			if(alpha > 45) {
+				ahead(lastX);
+			}
+			else {
+				ahead(lastY);
+			}
+		}
+                }
+	}
+    
+    
+     @Override
+	public void onMessageReceived(MessageEvent e) {
+            if(isTeammate(e.getSender())){
+		
+			String r = (String) e.getMessage();
+                        
+			String[] parts = r.split(":");
+                       
+			switch(parts[0]){
+			case "Atacar":  
+                            String[] parts2 = parts[1].split(",");
+                                    
+                                    this.atackBot(parts2[0],parts2[1]);
+                            
+				if(this.courage > 35) {
+                                  String[] parts1 = parts[1].split(",");
+                                    
+                                    this.atackBot(parts1[0],parts1[1]);
+                                }
+				else this.escape();
+				break;
+			case "Ajuda": if(this.courage > 35) {
+                            String[] parts1 = parts[1].split(",");
+                            this.helpGeneral(parts1[0],parts1[1]);
+                        }
+			else this.escape();
+				
+				break;
+			case "NE": this.nomeEnimigos.add(parts[1]);
+				break;
+			case "Aliado Morto":
+					this.courage = this.courage - this.personality;
+					break;
+			case "Enemigo Morto":
+				this.courage = this.courage + this.personality + 10;
+					break;
+			}
+				
+			
+	
+                        
+                              
+               }
+            
+	}
+    
+    
+    
+        private void escape() {
+	
+		   goTo();
+	}
+
+
+
+
+		private void helpGeneral(String string, String string2) {
+		int x = Integer.parseInt(string);
+		int y = Integer.parseInt(string2);
+                
+                double distancia=Math.sqrt(Math.pow(x-(int)getX(),2) + Math.pow(y-(int)getY(),2));
+
+                distanceRobot = Math.sqrt(Math.pow(60,2) + Math.pow(distancia,2));
+                angulo = Math.toDegrees(Math.atan(60/distancia));
+
+                turnLeft(angulo);
+               ahead(distanceRobot + 30);
+                
+                
+
+		
+	}
+
+
+
+
+private void atackBot(String string, String string2) {
+        int x = Integer.parseInt(string);
+        int y = Integer.parseInt(string2);
+        double distancia=Math.sqrt(Math.pow(x-(int)getX(),2) + Math.pow(y-(int)getY(),2));
+
+        distanceRobot = Math.sqrt(Math.pow(60,2) + Math.pow(distancia,2));
+        angulo = Math.toDegrees(Math.atan(60/distancia));
+
+        turnLeft(angulo);
+        //ahead(distanceRobot + 30);
+        smartFire(distanceRobot);
+        
+}
+
+
+
+
+		@Override
+    public void onRobotDeath(RobotDeathEvent e) {
+    
+        
+        if(this.nomeEnimigos.contains(e.getName()))
+            this.nomeEnimigos.remove(e.getName());
+        
+        System.out.println(this.nomeEnimigos.size());
+    
+    }
+    
+    
+    
+}
